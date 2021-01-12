eb7bfc8a8b2a3ba38fca4a29569bb7d1156d1269
Aaron Schneider <59979292+DrBraunvieh@users.noreply.github.com>
2021-01-12 07:25:58 +0100

    Add files via upload

--- LinkFilter.java
+++ LinkFilter.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -15,8 +16,32 @@ public class LinkFilter{
     private static final String REGULAR_EXPRESSION = "<a href=\"http://www.[a-z]*.[a-z]*\">[a-z]*</a>";
     
     public static void main(String[] args){
+        boolean weitermachen = true;
+        int numberOfLine = 0;
+        boolean hasFound;
+        String currentLine;
+        int anfangURL;
+        int endeURL;
+        int endeName;
+        
         try{
             Scanner file = new Scanner(new File(args[0]));
+            Pattern regex = Pattern.compile(REGULAR_EXPRESSION);
+            while(weitermachen){
+                currentLine = file.nextLine();
+                numberOfLine++;
+                Matcher match = regex.matcher(currentLine);
+                hasFound = match.find();
+                anfangURL = currentLine.indexOf("=\"");
+                endeURL = currentLine.indexOf("\">");
+                endeName = currentLine.indexOf("</a>");
+                if(hasFound == true){
+                    System.out.println(currentLine.substring(endeURL, endeName) + ": " + currentLine.substring(anfangURL, endeURL));
+                }
+                if(file.hasNextLine() == false){
+                    weitermachen = false;
+                }
+            }
         }catch(FileNotFoundException e){
             e.printStackTrace();
             System.out.println(FILE_NOT_FOUND);
@@ -24,6 +49,5 @@ public class LinkFilter{
             e.printStackTrace();
             System.out.println(NO_FILE_SPECIFIED);
         }
-        Pattern regex = Pattern.compile(REGULAR_EXPRESSION);
     }
 }
